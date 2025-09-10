d2d6728c1482eef8d3e608bc3e29e808d6a9d67c
ginacollecchia <ginacollecchia@gmail.com>
2025-09-10 11:53:32 -0400

    Initial Commit

---
+++ ConcertCam/ConcertCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcertCamApp.swift
+//  ConcertCam
+//
+//  Created by Gina Collecchia on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ConcertCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConcertCam/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ConcertCam
+//
+//  Created by Gina Collecchia on 9/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
